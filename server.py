3b94e8dfd6880cdf13fea9b42f2a1aa6de9a3184
DOR <dorber9@gmail.com>
2023-04-29 16:53:46 +0300

    cookie

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, render_template, redirect, session
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 import pymongo
 import requests
 import json
 from datetime import datetime, date
+from flask_session import Session
+
 
 
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 bcrypt = Bcrypt(app)
 api_token = "56243f10d28a4f3496e19f5e2ef61f50"
@@ -122,8 +127,8 @@ def signin():
     if result:
         if bcrypt.check_password_hash(result['password'], password): 
             resp = make_response({"result": 'success'})
-            resp.set_cookie('username', username)
-            return resp
+            session['username'] = username
+            return redirect("/")
         else:
             to_return['result'] = 'Wrong password!'
     else:
