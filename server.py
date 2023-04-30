6e02b109c85ca49e9f7e9bf1b02b53857b026544
Dorber9 <87997606+Dorber9@users.noreply.github.com>
2023-04-30 15:52:09 +0300

    Update server.py

--- server.py
+++ server.py
@@ -135,7 +135,6 @@ def signin():
         if bcrypt.check_password_hash(result['password'], password): 
             resp = make_response({"result": 'success', 'username': username})
             session_user['username'] = username
-            print(f"Session variable set to {session.get('username')}")
             return resp
         else:
             to_return['result'] = 'Wrong password!'
